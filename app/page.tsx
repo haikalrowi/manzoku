48cfacb5ff2113c38e3bc0a577e3f288c0103014
Haikal Rowi <93899076+haikalrowi@users.noreply.github.com>
2024-06-13 09:14:40 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+"use client";
+
+import { Spinner } from "@nextui-org/react";
+import { useEffect } from "react";
+
 export default function App() {
+  const manzoku =
+    "https://github.com/haikalrowi/satisfying-command/blob/main/README.md";
+  const redirectToManzoku = () => {
+    window.location.assign(manzoku);
+  };
+  useEffect(() => {
+    setTimeout(redirectToManzoku, 1000);
+  }, []);
   return (
-    <div>
-      <div>App</div>
+    <div className="flex h-screen items-center justify-center">
+      <Spinner label={`Redirecting to ${manzoku}`} />
     </div>
   );
 }
