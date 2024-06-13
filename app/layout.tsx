1da44dce953b288235488934fe0ed509e581005b
Haikal Rowi <93899076+haikalrowi@users.noreply.github.com>
2024-06-13 08:27:12 +0700

    587359

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import { NextUIProvider } from "@nextui-org/react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NextUIProvider>{children}</NextUIProvider>
+      </body>
     </html>
   );
 }
